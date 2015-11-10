305e56e00ebec9415091d8e6770d25e78d5762dd
Jerrod Junker <unsafepond@gmail.com>
2015-11-10 11:35:00 -0700

    initial commit

---
+++ db/migrate/20151110164604_create_users_table.rb
@@ -0,0 +1,13 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users_tables do |t|
+      t.string :uid
+      t.string :screen_name
+      t.string :image_url
+      t.string :oauth_token
+      t.string :oauth_token_secret
+
+      t.timestamps null: false
+    end
+  end
+end
