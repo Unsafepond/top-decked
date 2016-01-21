562688954fa5a6c88600a9f302a92d913af2349b
Jerrod Junker <unsafepond@gmail.com>
2016-01-21 06:02:14 -0700

    can hide based on mana cost

--- app/assets/javascripts/deck_creation.js
+++ app/assets/javascripts/deck_creation.js
@@ -3,4 +3,59 @@ $(document).ready(function(){
     var cardID = this.id;
     $('#card-id').val(cardID);
   })
+
+  $("#uButton").click( function(){ 
+    $(this).toggleClass("disabled");
+
+    $('tr td.mana').each(function() {
+        if ($(this).text().includes("{U}"))
+        {
+            $(this).parent().hide();
+        }
+    });
+  });
+
+  $("#gButton").click( function(){ 
+    $(this).toggleClass("disabled");
+
+    $('tr td.mana').each(function() {
+        if ($(this).text().includes("{G}"))
+        {
+            $(this).parent().hide();
+        }
+    });
+  });
+
+  $("#rButton").click( function(){ 
+    $(this).toggleClass("disabled");
+
+    $('tr td.mana').each(function() {
+        if ($(this).text().includes("{R}"))
+        {
+            $(this).parent().hide();
+        }
+    });
+  });
+
+  $("#wButton").click( function(){ 
+    $(this).toggleClass("disabled");
+
+    $('tr td.mana').each(function() {
+        if ($(this).text().includes("{W}"))
+        {
+            $(this).parent().hide();
+        }
+    });
+  });
+
+  $("#bButton").click( function(){ 
+    $(this).toggleClass("disabled");
+
+    $('tr td.mana').each(function() {
+        if ($(this).text().includes("{B}"))
+        {
+            $(this).parent().hide();
+        }
+    });
+  });
 });
